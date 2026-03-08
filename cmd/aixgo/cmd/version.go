0a3b1855ffb38d5492fa10a680dd0d318755a861
Charles Green <hello@charles.green>
2026-03-09 00:40:37 +0900

    fix: use Go build info for version in go install

--- cmd/aixgo/cmd/version.go
+++ cmd/aixgo/cmd/version.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"runtime"
+	"runtime/debug"
 
 	"github.com/spf13/cobra"
 )
@@ -12,8 +13,22 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version number of aixgo",
 	Long:  `Print the version number of aixgo, along with OS and architecture.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("aixgo version %s %s/%s\n", Version, runtime.GOOS, runtime.GOARCH)
+		fmt.Printf("aixgo version %s %s/%s\n", getVersion(), runtime.GOOS, runtime.GOARCH)
 	},
+}
+
+// getVersion returns the version string, preferring build info from go install.
+func getVersion() string {
+	if Version != "dev" && Version != "" {
+		return Version
+	}
+
+	// Fall back to build info (populated by go install)
+	if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "" && info.Main.Version != "(devel)" {
+		return info.Main.Version
+	}
+
+	return Version
 }
 
 func init() {
